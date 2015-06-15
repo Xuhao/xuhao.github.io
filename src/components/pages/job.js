d8d74f7d54f4811782fe9dedd90c81dcc36ee3f6
Xuhao <xuhao@rubyfans.com>
2015-06-15 21:54:23 +0800

    Change job page

--- src/components/pages/job.js
+++ src/components/pages/job.js
@@ -2,13 +2,24 @@
 import React from "react";
 
 const Job = React.createClass({
+  componentDidMount() {
+    const scriptOne = document.createElement('script');
+    const scriptTwo = document.createElement('script');
+    const wrapper = document.getElementsByName("linkedin_profile")[0];
+    scriptOne.type = 'text/javascript';
+    scriptOne.src = '//platform.linkedin.com/in.js';
+    scriptTwo.type = 'IN/MemberProfile';
+    scriptTwo.dataset.id = 'https://www.linkedin.com/pub/hao-xu/79/948/984';
+    scriptTwo.dataset.format = 'inline';
+    wrapper.appendChild(scriptOne);
+    wrapper.appendChild(scriptTwo);
+  },
+
   render() {
     return (
       <div id="job">
         <h1>我的工作</h1>
-        <img src="assets/images/xuhao.png" className="my-photo" />当前，我是易空海(Ekohe)的一名Web开发工程师。
-        <br />
-        <a href="http://ekohe.com/people#hao" target="_blank">http://ekohe.com/people#hao</a>
+        <div name="linkedin_profile"></div>
       </div>
     );
   }
