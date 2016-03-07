f8d298cbdf8dd564225f6de5c4e43128c66a361f
Xuhao <xuhao@rubyfans.com>
2016-03-07 22:07:07 +0800

    TMP

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
-"use strict";
+const webpack = require('webpack');
+const DEBUG = (process.argv.indexOf('--release') === -1);
+
+
 module.exports = {
   entry: "./main.js",
   output: {
     path: __dirname,
     filename: "bundle.js"
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
+      __DEV__: DEBUG,
+      'process.env.BROWSER': true
+    })
+  ],
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      { test: /\.js$/, loader: 'babel-loader' }
     ]
   }
 };
