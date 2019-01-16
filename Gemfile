149fc41c21015eb694a5407bd76a7279b11e658f
Xuhao <xuhao@rubyfans.com>
2019-01-16 08:22:33 +0800

    react app skeleton

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://ruby.taobao.org/'
-
-gem 'capistrano', '~> 3.4.0'
-gem 'capistrano-npm'
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './core/routes';
-import './assets/css/main.css';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-render(
-  <Router history={browserHistory} routes={routes} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
