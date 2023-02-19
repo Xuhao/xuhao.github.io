eb7ef0505dfaad6a1efeb1e6189bd4fd1d8d116f
xuhao@rubyfans.com <william.k8s.dev@gmail.com>
2023-02-20 00:41:53 +0800

    cleanup legacy code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="logo" src={logo} alt="avatar" />
+        <p>Xuhao</p>
+      </header>
+      <div>golang / nodejs / react / k8s / devops</div>
+      <div className="github-link">
+        <a href="https://github.com/Xuhao">@github</a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
